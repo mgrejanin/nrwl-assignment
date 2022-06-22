42fc15d31ee2f4e918800c10880c735f476912d7
Mauricio Grejanin <junior310595@gmail.com>
2022-06-22 19:33:37 -0300

    feature(core): adding husky

--- libs/tickets/feature/src/lib/tickets-feature.module.ts
+++ libs/tickets/feature/src/lib/tickets-feature.module.ts
@@ -1,3 +1,4 @@
+import { TicketsDataAccessModule } from '@acme/tickets/data-access';
 import { TicketsUiModule } from '@acme/tickets/ui';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -8,6 +9,7 @@ import { FeatureListComponent } from './feature-list/feature-list.component';
   imports: [
     CommonModule,
     TicketsUiModule,
+    TicketsDataAccessModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: FeatureListComponent },
     ]),
